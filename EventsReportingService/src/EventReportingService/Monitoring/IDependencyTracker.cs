6a7c0e5862a954f4e80458097de414c60c65af67
Anshum Soni <anshumsoni@yahoo.com>
2018-12-12 10:25:38 +0530

    removing event folder

--- EventsReportingService/src/EventReportingService/Monitoring/IDependencyTracker.cs
+++
@@ -1,9 +0,0 @@
-namespace EventReportingService.Monitoring
-{
-    using System;
-
-    public interface IDependencyTracker
-    {
-        void TrackDependency(string dependencyName, string commandName, DateTime startTime, TimeSpan duration, bool isSuccess);
-    }
-}
